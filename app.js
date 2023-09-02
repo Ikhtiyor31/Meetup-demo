cd509564c07cd1ceb8120fd279054d825f017ce0
abdul <55629713+Ikhtiyor31@users.noreply.github.com>
2023-09-03 02:23:55 +0900

    nodejs server demo

---
+++ app.js
@@ -0,0 +1,52 @@
+import express from "express";
+const app = express();
+app.use(express.json());
+
+import User from "./user.js";
+
+app.get("/server", (req, res) => {
+  return res.send("<h1>Web server is working</h1>");
+});
+
+app.get("/api/v1/user", (req, res) => {
+  return res.send(`name: ${req.query.name}`);
+});
+
+var userDB = new Map();
+app.post("/api/v1/users/signup", (req, res) => {
+  const { username, password } = req.body;
+  if (username == undefined || password == undefined) {
+    return res
+      .status(400)
+      .send(`unexprected request fields ${JSON.stringify(req.body)}`);
+  }
+
+  const user = new User(username, password);
+  if (userDB.has(username)) {
+    return res.status(400).send("user already exists.!");
+  }
+  userDB.set(user.username, user);
+  return res.status(200).send(user);
+});
+
+app.post("/api/v1/users/signin", (req, res) => {
+  const { username, password } = req.body;
+  if (username == undefined || password == undefined) {
+    return res
+      .status(400)
+      .send(`unexprected request fields ${JSON.stringify(req.body)}`);
+  }
+
+  if (!userDB.has(username)) {
+    return res.status(400).send("the username is wrong!");
+  }
+
+  if (userDB.get(username).password != password) {
+    return res.status(400).send("the password is wrong!");
+  }
+
+  return res.status(200).send(`user login is successfull`);
+});
+
+const PORT = 3000;
+app.listen(PORT, () => console.log(`server is listening on ${PORT}`));
